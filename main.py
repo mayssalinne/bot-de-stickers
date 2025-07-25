9d1eab00932d050975a41361696678231a0b8e8a
mayssalinne <mayssalinne@gmail.com>
2025-07-25 16:10:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-import os
-
-TOKEN = os.getenv("7956634632:AAFijJtLbal-wJxZ8eb4m9u1a69hvBat1Tc")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Olá, eu sou seu bot de stickers! Use /newpack para começar 👑")
+    await update.message.reply_text("Olá! Eu sou seu bot de stickers! Use /newpack para começar 👑")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
